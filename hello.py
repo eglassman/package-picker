6181ab78c7e516eaef4305c6431b48399717318a
Elena Glassman <elena.glassman@gmail.com>
2017-05-18 19:36:09 -0700

    introduced templates, added bootstrap

--- hello.py
+++ hello.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route('/')
 def hello_world():
-    return 'Not dead. Yet.'
+    #return 'Not dead. Yet.'
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
